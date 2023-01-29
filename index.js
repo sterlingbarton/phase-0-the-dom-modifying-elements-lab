9dcd2ac43d88b4fc5ba117815fff34b00a02f8c3
Sterling <sterlingbarton81@gmail.com>
2023-01-29 14:40:46 -0600

    commit tree 455f5248db7ad22585b69751227e7128e2500116

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.querySelector('#main');
+main.remove();
+
+// describe("after index.js is processed", () => {
+//     it("no longer has DOM node 'main#main'", () => {
+//       expect(document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+//     });
